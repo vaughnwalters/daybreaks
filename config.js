579d77cee2c7a47d663aa6f15172c5973ae99aaf
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-03 00:25:04 -0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  consumer_key:         '7qh0sBo0SYIwElbxzYYoRXND1',
-  consumer_secret:      'tQo3oOpl7wImfGpKUXt89htd1R91a0qQsJsONkAzGwjWCZsAXF',
-  access_token:         '782761225033703424-7uG30ufLQ0Lu9F9CCp3P5hLzo7ZuIAK',
-  access_token_secret:  'oFLfeQ9UyX3uPlygX6KAB2iHgZ0c7UnByl2QmqipnjZO9',
-}
